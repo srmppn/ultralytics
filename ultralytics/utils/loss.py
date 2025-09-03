a045050919ff6ed1dd4db730512fc6e631454d5b
somrak.mon <somrak.mon@ascendcorp.com>
2025-09-04 00:26:31 +0700

    checkpoint

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -279,25 +279,22 @@ class v8DetectionLoss:
             mask_gt,
         )
 
-        # ===== Only for investigation =====
-        # print('size bbox', len(batch['img']))
         b, c, h, w = batch['img'].shape
         upsampler_box = []
         for i in range(b):
-            gsd = batch['gsd'][i].item()
+            gsd = batch['gsd'][i].clone().item()
             bbx = target_bboxes[i]
             upsampler_box.append(scale_boxes_with_padding(bbx, gsd, h, w))
         target_bboxes = torch.stack(upsampler_box, dim=0)
-        #print('result', r.shape, target_bboxes.shape)
+        # print('result', r.shape, target_bboxes.shape)
         # b, c, h, w = batch['img'].shape
-        # print('target boxes:', target_bboxes)
         # for i in range(b):
-        #     img = batch['img'][i].unsqueeze(0)
-        #     gsd = batch['gsd'][i].item()
-        #     bbx = target_bboxes[i]
-        #     bbx = scale_boxes_with_padding(bbx, gsd, h, w)
-        #     img = scale_with_padding(img, gsd).squeeze(0)  # [3, 640, 640] (remove batch dim)
-        #     img = img.permute(1, 2, 0).cpu().numpy()  # [640, 640, 3]  (CHW -> HWC)
+        #     img = batch['img'][i].clone().unsqueeze(0).detach()
+        #     gsd = batch['gsd'][i]
+        #     # bbx = target_bboxes[i]
+        #     bbx = scale_boxes_with_padding(target_bboxes[i], 0.5, h, w)
+        #     img = scale_with_padding(img, torch.tensor([0.5])).squeeze(0)  # [3, 640, 640] (remove batch dim)
+        #     img = img.permute(1, 2, 0).cpu().detach().numpy()  # [640, 640, 3]  (CHW -> HWC)
         #     # If it's float (0-1), scale to 0-255
         #     if img.max() <= 1.0:
         #         img = (img * 255).astype("uint8")
@@ -306,7 +303,7 @@ class v8DetectionLoss:
         #
         #     # Convert RGB -> BGR for OpenCV
         #     img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        #     print('what bbx', bbx)
+        #     # print('what bbx', bbx)
         #     x1, y1, x2, y2 = bbx[0].int().tolist()
         #     cv2.rectangle(img_bgr, (x1, y1), (x2, y2), color=(0, 255, 0), thickness=2)
         #
--- ultralytics/utils/transform.py
+++ ultralytics/utils/transform.py
@@ -3,23 +3,46 @@ import torch.nn.functional as F
 
 def scale_with_padding(img, scale):
     b, c, h, w = img.shape
-    resize_h, resize_w = int(round(h * scale)), int(round(w * scale))
 
-    if resize_h == 0 or resize_w == 0:
-        return torch.zeros((b, c, h, w), dtype=img.dtype, device=img.device)
+    y_coords = torch.linspace(-1, 1, h, device=img.device)
+    x_coords = torch.linspace(-1, 1, w, device=img.device)
 
-    # Compute padding
-    pad_h = h - resize_h
-    pad_w = w - resize_w
+    grid_y, grid_x = torch.meshgrid(y_coords, x_coords, indexing='ij')
 
-    # Divide padding into 2 sides
-    pad_top = pad_h // 2
-    pad_bottom = pad_h - pad_top
-    pad_left = pad_w // 2
-    pad_right = pad_w - pad_left
+    # grid_y = grid_y / scale
+    # grid_x = grid_x / scale
 
-    img_resized = F.interpolate(img, size=(resize_h, resize_w), mode="bilinear", align_corners=False)
-    return F.pad(img_resized, (pad_left, pad_right, pad_top, pad_bottom), value=114/255.0)
+    # print('check grid', grid_y)
+    # print('check grid', grid_x)
+    # Stack and reshape for grid_sample
+    # grid_sample expects (B, H, W, 2) where last dim is [x, y]
+    grid = torch.stack([grid_x, grid_y], dim=-1)  # (H, W, 2)
+    grid = grid.unsqueeze(0).expand(b, -1, -1, -1)  # (B, H, W, 2)
+
+    scale = scale.view(b, 1, 1, 1)  # [B,1,1,1]
+    grid = grid / scale  # shrinks grid coordinates
+
+    # Sample using grid_sample
+    scaled_image = F.grid_sample(img, grid, mode='bilinear')
+    return scaled_image
+    return scaled_image
+    # resize_h, resize_w = int(round(h * scale)), int(round(w * scale))
+    #
+    # if resize_h == 0 or resize_w == 0:
+    #     return torch.zeros((b, c, h, w), dtype=img.dtype, device=img.device)
+    #
+    # # Compute padding
+    # pad_h = h - resize_h
+    # pad_w = w - resize_w
+    #
+    # # Divide padding into 2 sides
+    # pad_top = pad_h // 2
+    # pad_bottom = pad_h - pad_top
+    # pad_left = pad_w // 2
+    # pad_right = pad_w - pad_left
+    #
+    # img_resized = F.interpolate(img, size=(resize_h, resize_w), mode="bilinear", align_corners=False)
+    # return F.pad(img_resized, (pad_left, pad_right, pad_top, pad_bottom), value=114/255.0)
 
 def scale_boxes_with_padding(boxes, scale, h, w):
     """

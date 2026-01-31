b8469688dbd80f468dcaf2a3982b33dd4824b902
somrak.mon <somrak.mon@ascendcorp.com>
2026-02-01 00:15:00 +0700

    try different model

--- ultralytics/utils/checks.py
+++ ultralytics/utils/checks.py
@@ -779,7 +779,7 @@ def check_amp(model):
     try:
         from ultralytics import YOLO
 
-        assert amp_allclose(YOLO("yolo11n.pt"), im)
+        assert amp_allclose(YOLO("ultralytics/cfg/models/11/yolo11-adaptive-resize.yaml"), im)
         LOGGER.info(f"{prefix}checks passed ✅")
     except ConnectionError:
         LOGGER.warning(f"{prefix}checks skipped. Offline and unable to download YOLO11n for AMP checks. {warning_msg}")

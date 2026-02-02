6770c9d2c30cce9eb48ed646c916e6156c510a31
somrak.mon <somrak.mon@ascendcorp.com>
2026-02-02 18:30:40 +0700

    correcting target bbox

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -360,7 +360,7 @@ class v8DetectionLoss:
 #             print('check var', variance)
 #             loss[3] = torch.log(variance + eps)
 
-            target_bboxes /= stride_tensor
+            # target_bboxes /= stride_tensor
             loss[0], loss[2] = self.bbox_loss(
                 pred_distri, pred_bboxes, anchor_points, target_bboxes, target_scores, target_scores_sum, fg_mask
             )
